c7ab2cb3c6e974399b4dd3dddcb80249422bf3a9
Matt Mansour <slackbabbath@gmail.com>
2012-10-07 17:24:45 -0700

    Seperating Get Subs and Get Views

--- ytdata/management/commands/sb_get_top_views.py
+++ ytdata/management/commands/sb_get_top_views.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         top_100_page=urllib2.urlopen(TOP_100_URL)
         sbtopsoup=BeautifulSoup(top_100_page.read())
-        
+
         table = sbtopsoup.find('table')
         rows = table.findAll('tr')
         row_count = 0
@@ -22,20 +22,26 @@ class Command(BaseCommand):
         for tr in rows:
             row_count += 1
             row = tr.findAll('a',text=True)
-            
+
             if row_count > 2:
-################# GET TOP 100 # OF SUBSCIBERS
-                yt, created = YouTuber.objects.get_or_create(title=str(row[2]))
-                print row[2]
-
-
-        
-
-
-
-
-
+################ GET TOP 100 # OF TOTAL CHANNEL VIEWS
+                try:
+                    yt = YouTuber.objects.get(title=str(row[6]))
+#                    yt.save()
+                except YouTuber.DoesNotExist:
+                    yt = YouTuber(title=str(row[6]),status=2)
+                    yt.save()
+                print row[6]
 
 
 
 
+
+
+
+
+
+
+
+
+  
