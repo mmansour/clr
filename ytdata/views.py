9a907ab1407b21e7111c0cd6e5ddfd8a0a4f79ec
Matt Mansour <slackbabbath@gmail.com>
2012-10-09 13:47:08 -0700

    Stats Templates

--- ytdata/views.py
+++ ytdata/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from ytdata.models import YouTuber
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
+from django.template import RequestContext
+#from django.http import HttpResponse, Http404, HttpResponsePermanentRedirect
+
+def home(request):
+#    top_ten_subs = YouTuber.objects.filter(status=2).order_by('-youtube_subscribers')[:10]
+    return render_to_response('index.html',
+                       {},
+                        context_instance=RequestContext(request))
+
+def yt_top_ten_subs(request):
+    top_ten_subs = YouTuber.objects.filter(status=2).order_by('-youtube_subscribers')[:10]
+    return render_to_response('pages/yt-top-ten-subs.html',
+                       {'top_ten_subs':top_ten_subs},
+                        context_instance=RequestContext(request))
