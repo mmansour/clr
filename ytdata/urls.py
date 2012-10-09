37e7219bc104919fb254074bbf253bbccbe7f3e8
Matt Mansour <slackbabbath@gmail.com>
2012-10-09 13:51:12 -0700

    Clean up

---
+++ ytdata/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
+
+from mezzanine.core.views import direct_to_template
+
+urlpatterns = patterns('ytdata.views',
+    url("^$", "home", name="home"),
+    (r'^youtube-top-10-subscribed-channels/$', 'yt_top_ten_subs'),
+#    (r'^(?P<pageslug>[\w-]+)-(?P<hotelid>.*)/$', 'detail'),
+)
+
