ab490bbd111a63821660d678929177e8f94c06f4
Matt Mansour <slackbabbath@gmail.com>
2012-10-09 16:07:01 -0700

    Changing the youtube landing page to a view instead of mezz

--- ytdata/urls.py
+++ ytdata/urls.py
@@ -7,6 +7,8 @@ from mezzanine.core.views import direct_to_template
 urlpatterns = patterns('ytdata.views',
     url("^$", "home", name="home"),
     (r'^youtube-top-10-subscribed-channels/$', 'yt_top_ten_subs'),
+    (r'^youtube-stats/$', 'yt_stats_landing'),
+
 #    (r'^(?P<pageslug>[\w-]+)-(?P<hotelid>.*)/$', 'detail'),
 )
 
--- ytdata/views.py
+++ ytdata/views.py
@@ -9,6 +9,12 @@ def home(request):
                        {},
                         context_instance=RequestContext(request))
 
+def yt_stats_landing(request):
+#    top_ten_subs = YouTuber.objects.filter(status=2).order_by('-youtube_subscribers')[:10]
+    return render_to_response('pages/yt-stats-landing.html',
+                       {},
+                        context_instance=RequestContext(request))
+
 def yt_top_ten_subs(request):
     top_ten_subs = YouTuber.objects.filter(status=2).order_by('-youtube_subscribers')[:10]
     return render_to_response('pages/yt-top-ten-subs.html',
